007915f05e1f090b7bcde3f0d2864d8ee1259b0d
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-07-01 20:25:53 -0500

    Integracion de CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Configuración de CORS
+  // Permitir solicitudes desde cualquier origen, puedes ajustar esto según tus necesidades
+  app.enableCors({
+    origin: '*', // Permitir todas las solicitudes CORS
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Accept', // Encabezados permitidos
+    credentials: true, // Permitir credenciales (cookies, autenticación HTTP, etc.)
+  });
+
+  // Validaciones globales
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Elimina propiedades no permitidas
+    forbidNonWhitelisted: true, // Lanza un error si se envían propiedades no permitidas
+    transform: true, // Transforma los datos entrantes a sus tipos correspondientes
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
