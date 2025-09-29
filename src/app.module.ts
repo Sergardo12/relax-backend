91075a24cd713c3e1ebdf5396a589e726d81a2b6
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-09-29 09:49:52 -0500

    feat: Integracion del modulo de Rol y Usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolModule } from './modules/Rol/rol.module';
+import { UsuarioModule } from './modules/Usuario/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'relaxdb',
+      autoLoadEntities: true,
+      synchronize: true, // Solo para desarrollo, en producción usar migraciones
+    }),
+    RolModule,
+    UsuarioModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
