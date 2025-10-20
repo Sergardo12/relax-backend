480ebb2bc446a01fd87b3f0dad7d46ac465a29ab
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-10-20 01:25:55 -0500

    feat(app): Integracion de todos los modulos al modulo padre

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RolModule } from './modules/Rol/rol.module';
-import { UsuarioModule } from './modules/Usuario/usuario.module';
+import { PacienteModule } from './modules/paciente/paciente.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { EspecialidadModule } from './modules/especialidad/especialidad.module';
+import { ColaboradorModule } from './modules/colaborador/colaborador.module';
+import { ServicioModule } from './modules/servicio/servicio.module';
+import { CitaModule } from './modules/cita/cita.module';
+import { DetalleCitaModule } from './modules/detalle-cita/detalle-cita.module';
+import { HorarioColaboradorModule } from './modules/horario-colaborador/horario-colaborador.module';
+import { PagoCitaModule } from './modules/pago-cita/pago-cita.module';
+import { TratamientoModule } from './modules/tratamiento/tratamiento.module';
+import { SesionTratamientoModule } from './modules/sesion-tratamiento/sesion-tratamiento.module';
+import { MembresiaModule } from './modules/membresia/membresia.module';
+import { SuscripcionModule } from './modules/suscripcion/suscripcion.module';
+import { PagoSuscripcionModule } from './modules/pago-suscripcion/pago-suscripcion.module';
+import { BeneficioMembresianModule } from './modules/beneficio-membresia/beneficio-membresia.module';
+import { ConsumoBeneficioModule } from './modules/consumo-beneficio/consumo-beneficio.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { RolModule } from './modules/rol/rol.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -17,8 +35,24 @@ import { UsuarioModule } from './modules/Usuario/usuario.module';
       autoLoadEntities: true,
       synchronize: true, // Solo para desarrollo, en producción usar migraciones
     }),
+    AuthModule,
     RolModule,
-    UsuarioModule
+    UsuarioModule,
+    PacienteModule,
+    EspecialidadModule,
+    ColaboradorModule,
+    ServicioModule,
+    CitaModule,
+    DetalleCitaModule,
+    HorarioColaboradorModule,
+    PagoCitaModule,
+    TratamientoModule,
+    SesionTratamientoModule,
+    MembresiaModule,
+    BeneficioMembresianModule,
+    SuscripcionModule,
+    ConsumoBeneficioModule,
+    PagoSuscripcionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
